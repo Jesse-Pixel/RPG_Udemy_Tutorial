db71a2e1292ff11b6f54def6af1415dd6d20a827
jesseleerader <jesse.lee.rader@gmail.com>
2020-04-06 00:33:24 -0400

    Implemented simple NavMesh abilities with a test target for the player capsule to follow.

---
+++ Assets/Mover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+
+    public Transform target;
+
+    private Vector3 targetPosition;
+    private NavMeshAgent navMesh;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPosition = target.position;
+        navMesh = GetComponent<NavMeshAgent>();
+        navMesh.destination = targetPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
